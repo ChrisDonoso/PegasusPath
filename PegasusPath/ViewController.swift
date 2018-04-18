7c373d0690af975090f5e4b7e632ea15f5ee2bb1
Huey Padua <yeuhpadua@yahoo.com>
2018-04-18 14:13:02 -0400

    Register controller added and password confirmation

--- PegasusPath/ViewController.swift
+++ PegasusPath/ViewController.swift
@@ -10,31 +10,23 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
-    
-    @IBOutlet weak var signinSelector: UISegmentedControl!
-    
-    @IBOutlet weak var signinLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
     
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBOutlet weak var firstTextField: UITextField!
-    
-    @IBOutlet weak var lastTextField: UITextField!
-    
     @IBOutlet weak var signinButton: UIButton!
     
-    var isSignIn:Bool = true
     
-    var docRef: DocumentReference!
+//    var docRef: DocumentReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstTextField.isHidden = true
-        lastTextField.isHidden = true
+//        firstTextField.isHidden = true
+//        lastTextField.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,83 +34,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func signinSelectorChanged(_ sender: Any) {
-        
-        //Flip the boolean
-        isSignIn = !isSignIn
-        
-        //Check the bool and set the button and labels
-        if isSignIn {
-            signinButton.setTitle("Login", for: .normal)
-            firstTextField.isHidden = true
-            lastTextField.isHidden = true
-        }
-        else  {
-            signinButton.setTitle("Register", for: .normal)
-            firstTextField.isHidden = false
-            lastTextField.isHidden = false
-        }
-    }
+//    @IBAction func signinSelectorChanged(_ sender: Any) {
+//
+//        //Flip the boolean
+//        isSignIn = !isSignIn
+//
+//        //Check the bool and set the button and labels
+//        if isSignIn {
+//            signinButton.setTitle("Login", for: .normal)
+//            firstTextField.isHidden = true
+//            lastTextField.isHidden = true
+//        }
+//        else  {
+//            signinButton.setTitle("Register", for: .normal)
+//            firstTextField.isHidden = false
+//            lastTextField.isHidden = false
+//        }
+//    }
     
     @IBAction func signinButtonTapped(_ sender: Any) {
         
         //TODO: Do some form validation on the email and password
-        
         if let email = emailTextField.text, let pass = passwordTextField.text, email != "", pass != "" {
             
-            //Check if it's sign in or register
-            if isSignIn {
                 //Sign in the user with Firebase
                 Auth.auth().signIn(withEmail: email, password: pass, completion: { (user, error) in
                     
                     //Check  that user isn't nil
                     if let u = user {
                         //user is found, go to home screen
-                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                        self.performSegue(withIdentifier: "loginToHome", sender: self)
                     }
                     else {
                         AlertController.showAlert(self, title: "Error", message: (error?.localizedDescription)!)
                         return
                     }
                 })
-            }
-            else {
-                //Register the user with Firebase
-                if let email = emailTextField.text, let pass = passwordTextField.text, let first = firstTextField.text, let last = lastTextField.text, email != "", pass != "", first != "", last != "" {
-                    
-                    Auth.auth().createUser(withEmail: email, password: pass, completion: { (user, error) in
-                        
-                        // Add user information to firestore database
-                        let docRef = Firestore.firestore().collection("users").document()
-                        let userID = docRef.documentID
-                        
-                        docRef.setData(["email": email, "firstname": first, "lastname": last, "uid": userID])
-                    /*
-                    // Check to see if data has been saved to database
-                    self.docRef.setData() { (error) in
-                        if let error = error {
-                            print("Error: \(error.localizedDescription)")
-                        }
-                        else {
-                            print("Data has been saved.")
-                        }
-                    }
-                    */
-                    //check that user isn't nil
-                        if let u = user {
-                            //user is found, go to homescreen
-                            self.performSegue(withIdentifier: "goToHome", sender: self)
-                        }
-                        else {
-                            //Error: check error and show message
-                        }
-                    })
-                }
-                else {
-                    AlertController.showAlert(self, title: "Missing Info", message: "Please fill out all field.")
-                    return
-                }
-            }
         }
         else {
             AlertController.showAlert(self, title: "Missing Info", message: "Please fill out all field.")
@@ -131,9 +82,95 @@ class ViewController: UIViewController {
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
+    @IBAction func LinkToRegisterTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToRegisterSegue", sender: self)
+    }
+}
+
+class RegisterController: UIViewController {
+    
+    @IBOutlet weak var firstTextField: UITextField!
+    
+    @IBOutlet weak var lastTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPassTextField: UITextField!
+    
+    var docRef: DocumentReference!
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        
+        //Register the user with Firebase
+        if let email = emailTextField.text,
+            let pass = passwordTextField.text,
+            let confirmPass = confirmPassTextField.text,
+            let first = firstTextField.text,
+            let last = lastTextField.text,
+            email != "",
+            pass != "",
+            first != "",
+            last != "",
+            confirmPass != "",
+            pass == confirmPass {
+            
+                Auth.auth().createUser(withEmail: email, password: pass, completion: { (user, error) in
+                
+                // Add user information to firestore database
+                let docRef = Firestore.firestore().collection("users").document()
+                let userID = docRef.documentID
+            
+                docRef.setData(["email": email, "firstname": first, "lastname": last, "uid": userID])
+                /*
+                // Check to see if data has been saved to database
+                 self.docRef.setData() { (error) in
+                 if let error = error {
+                 print("Error: \(error.localizedDescription)")
+                 }
+                 else {
+                 print("Data has been saved.")
+                 }
+                 }
+                 */
+                //check that user isn't nil
+                if let u = user {
+                    //user is found, go to homescreen
+                    self.performSegue(withIdentifier: "registerToHome", sender: self)
+                }
+                
+                else {
+                    //Error: check error and show message
+                    AlertController.showAlert(self, title: "Error", message: (error?.localizedDescription)!)
+                    return
+                }
+            })
+        }
+        else {
+            AlertController.showAlert(self, title: "Missing Info", message: "Please fill out all field.")
+            return
+        }
+    }
+    @IBAction func LinkToLoginTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "goToLoginSegue", sender: self)
+    }
 }
 
 class LogoutController: UIViewController {
+    
+    @IBOutlet weak var successMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user = Auth.auth().currentUser
+        if let user = user {
+            let email = user.email
+            
+            successMessage.text = "Hello, \(email!)"
+        }
+    }
     
     @IBAction func signoutButtonTapped(_ sender: Any) {
         do {
